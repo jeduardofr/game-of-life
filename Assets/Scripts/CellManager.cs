4325ca266589469db480063aba620791c9061d67
jeduardofr <jeduardo.fuentes@alumnos.udg.mx>
2022-05-23 19:17:17 -0500

    Progress with Simulation Logic

--- Assets/Scripts/CellManager.cs
+++ Assets/Scripts/CellManager.cs
@@ -5,14 +5,27 @@ using UnityEngine;
 
 public class CellManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private int m_Width, m_Height;
+
+    [SerializeField] private Cell m_CellPrefab;
+
+    [SerializeField] private Transform m_Camera;
     void Start()
     {
-        
+        GenerateGrid();
     }
 
-    // Update is called once per frame
-    void Update()
+    void GenerateGrid()
     {
+        for (int x = 0; x < m_Width; ++x)
+        {
+            for (int y = 0; y < m_Height; ++y)
+            {
+                var spawnedCell = Instantiate(m_CellPrefab, new Vector3(x, y), Quaternion.identity);
+                spawnedCell.name = $"Tile {x} {y}";
+            }
+        }
+
+        // m_Camera.transform.position = new Vector3((float)m_Width / 2 - 0.5f, (float)m_Width / 2 - 0.5f, 10);
     }
 }
--- Assets/Scripts/CustomSceneManager.cs
+++ Assets/Scripts/CustomSceneManager.cs
@@ -1,8 +1,24 @@
+using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class CustomSceneManager : MonoBehaviour
 {
+    private CustomSceneManager m_Instance;
+
+    private void Awake()
+    {
+        if (m_Instance == null)
+        {
+            m_Instance = this;
+            DontDestroyOnLoad(m_Instance);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
     public void LoadSimulation()
     {
         SceneManager.LoadScene("Simulation");
